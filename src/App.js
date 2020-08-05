3716a147b8925f154f1668c9fee48d6d697e831a
Román Lisin <roman.lisin@web.com>
2020-08-05 16:08:29 -0300

    Layout + basic form
    Create a form field with an icon
    Update the layout styles
    Import material styles
    Import material icons font
    Cleanup redundant styles
    Note: TextField[fullwidth] doesn't work well with an icon

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import { TextField } from '@rmwc/textfield';
+import { Icon } from '@rmwc/icon';
 import './App.css';
+import '@material/button/dist/mdc.button.css';
+import '@material/chips/dist/mdc.chips.css';
+import '@material/floating-label/dist/mdc.floating-label.css';
+import '@material/line-ripple/dist/mdc.line-ripple.css';
+import '@material/list/dist/mdc.list.css';
+import '@material/notched-outline/dist/mdc.notched-outline.css';
+import '@material/ripple/dist/mdc.ripple.css';
+import '@material/snackbar/dist/mdc.snackbar.css';
+import '@material/textfield/dist/mdc.textfield.css';
+import '@rmwc/icon/icon.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class="App-form">
+            <TextField icon="filter_alt" placeholder="Type to filter by city name or country"
+              style={{width: '100%'}}
+            />
+        </div>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://lisin.ru"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          &copy; 2020, Roman Lisin
         </a>
       </header>
     </div>
